807eea2ebd3e4b785beed92653b05e9db4bdd3e9
akanshamehta17 <akansha.mehta@sjsu.edu>
2017-03-16 11:56:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+import sys
+from github import Github
+
+url = sys.argv[1]
+user = url.split("/")
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Dockerized Flask App!!"
+@app.route('/v1/<filename>')
+def show_file_contents(filename):
+ g = Github("akanshamehta17","P@ssw0rd")
+ repo = g.get_user(user[3]).get_repo(user[4])
+ try:
+   file = repo.get_file_contents(filename)
+   if file is not None:
+     if(filename.endswith(".yml")): 
+        file = repo.get_file_contents(filename)
+        return file.decoded_content
+     if(filename.endswith(".json")):
+        file = repo.get_file_contents(filename)
+        return file.decoded_content
+
+ except:
+   return "Error: File not found" 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+  app.run(debug=True,host='0.0.0.0')
+
